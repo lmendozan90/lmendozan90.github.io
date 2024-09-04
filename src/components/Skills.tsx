77902d1c5b0e52660a422963d4f7be48e660adc5
lmendozan90 <lmendozan90@gmail.com>
2024-09-04 16:14:10 -0500

    Add Skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,91 +1,63 @@
 import * as React from "react";
-import { HiStar } from "react-icons/hi";
-
-const Stars = ({ count }: { count: Number }) => {
-  return (
-    <span>
-      {[...Array(count)].map((_, i) => (
-        <HiStar key={i} className="inline text-yellow-500" />
-      ))}
-    </span>
-  );
-};
-
-const SkillLabel = ({ name }: { name: String }) => {
-  return (
-    <div className="bg-[#E96479] rounded-md p-1 shadow-sm text-center self-center">
-      <h3 className="text-xs font-semibold flex justify-between text-[#DEF2F1]">
-        {name}
-        <Stars count={5} />
-      </h3>
-    </div>
-  );
-};
-
-const SkillSubSection = ({
-  title,
-  children,
-}: {
-  title: String;
-  children: React.ReactNode;
-}) => {
-  return (
-    <div>
-      <h3 className="text-xl md:text-2xl font-bold tracking-tight mt-3">
-        {title}
-      </h3>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 mt-3">
-        {children}
-      </div>
-    </div>
-  );
-};
+import AstronautIcon from "../assets/icons/astronautIcon";
 
 const Skills: React.FC = () => {
   return (
-    <section className="py-20 md:py-24" id="skills">
-      <div className="container mx-auto px-4 md:px-6 flex items-center justify-evenly">
-        <div className="space-y-2 text-center">
-          <h2 className="text-[4.4em] font-semibold font-mono leading-none tracking-tight text-[#4D455D] max-w-[24rem]">
-            Technical Skills
-          </h2>
+    <div className="border-t border-[#36f3d1] px-[30px] pt-8 pb-[90px]">
+      <div className="text-center flex flex-col items-center">
+        <div className="mx-auto mb-[27px]">
+          <AstronautIcon width="38" height="38" fill="currentColor" />
         </div>
-        <div className="text-[#4D455D]">
-          <SkillSubSection title="Programming Languages">
-            <SkillLabel name="Ruby" />
-            <SkillLabel name="Javascript" />
-            <SkillLabel name="Typescript" />
-            <SkillLabel name="HTML" />
-            <SkillLabel name="CSS" />
-          </SkillSubSection>
-          <SkillSubSection title="Frameworks & Libraries">
-            <SkillLabel name="Ruby on Rails" />
-            <SkillLabel name="React" />
-            <SkillLabel name="TailwindCSS" />
-          </SkillSubSection>
-          <SkillSubSection title="Build Tools">
-            <SkillLabel name="Webpack" />
-            <SkillLabel name="Vite" />
-          </SkillSubSection>
-          <SkillSubSection title="Testing Tools">
-            <SkillLabel name="RSpec" />
-            <SkillLabel name="Jest" />
-            <SkillLabel name="Cypress" />
-          </SkillSubSection>
-          <SkillSubSection title="Database Management">
-            <SkillLabel name="PostgreSQL" />
-            <SkillLabel name="MySQL" />
-          </SkillSubSection>
-          <SkillSubSection title="Version Control">
-            <SkillLabel name="Git" />
-          </SkillSubSection>
-          <SkillSubSection title="Cloud Services">
-            <SkillLabel name="AWS" />
-          </SkillSubSection>
-        </div>
+        <span className="text-4xl leading-[46px] font-bold">Skills</span>
       </div>
-    </section>
+      <section className="border-t border-[#36f3d1] mt-10 pt-10">
+        <div className="flex flex-col">
+          <div className="mb-5 font-bold">TECHNICAL SKILLS</div>
+          {SkillsDescription.map((skill, index) => (
+            <div key={index} className="flex flex-col mb-[25px]">
+              <span>{skill.name}</span>
+              <span className="font-bold">{skill.experience}</span>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   );
 };
+
+const SkillsDescription = [
+  {
+    name: "Ruby on Rails",
+    experience: "> 5 years",
+  },
+  {
+    name: "React",
+    experience: "> 5 years",
+  },
+  {
+    name: "AWS",
+    experience: "1.5 years",
+  },
+  {
+    name: "Heroku",
+    experience: "4 years",
+  },
+  {
+    name: "TailwindCSS & TailwindUI",
+    experience: "4 years",
+  },
+  {
+    name: "HTML & CSS",
+    experience: "> 6 years",
+  },
+  {
+    name: "Javascript",
+    experience: "> 5 years",
+  },
+  {
+    name: "Ruby",
+    experience: "> 5 years",
+  },
+];
 
 export default Skills;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,15 @@
 import * as React from "react";
 import General from "../components/General";
 import WorkExperience from "../components/WorkExperience";
-import RocketIcon from "../assets/icons/rocketIcon";
 import Skills from "../components/Skills";
-import Contact from "../components/Contact";
 
 const Home: React.FC = () => {
   return (
     <div className="">
       <General />
       <WorkExperience />
+      <Skills />
       {/*<main className="relative h-screen overflow-y-scroll">*/}
-      {/*  <div className="">*/}
-      {/*    <Skills />*/}
-      {/*  </div>*/}
       {/*  <div className="">*/}
       {/*    <Contact />*/}
       {/*  </div>*/}
